0f541dac9757b6eb850b1a0994023d69f6767cec
Guru Prasath <95870736+GURUPRASATH-OG@users.noreply.github.com>
2024-05-25 02:22:41 +0530

    get year data

--- scripts/year.js
+++ scripts/year.js
@@ -1,8 +1,2 @@
-fetch('http://worldtimeapi.org/api/timezone/Asia/Kolkata')
-    .then((data) => {
-        return data.json();
-    })
-    .then((date) => {
-        let year = new Date(date.datetime);
-        document.getElementById("year").innerHTML = year.getFullYear()
-    });
+const date = new Date();
+document.getElementById("year").innerHTML = date.getFullYear();
