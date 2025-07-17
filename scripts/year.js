1e8179643371a4b196fc5b159b8a0feb54668170
krishna subramaniyan <krishnasubramaniyan2001@gmail.com>
2025-07-17 19:34:55 +0530

    fix year api

--- scripts/year.js
+++ scripts/year.js
@@ -1,40 +1,45 @@
-var data = null;
-let yearData = JSON.parse(localStorage.getItem('yearData'));
-const sysDateTime = new Date();
+const cached = JSON.parse(localStorage.getItem("yearData"));
+const sysTime = new Date();
 
-if(!yearData || (sysDateTime.getTime() > yearData.expiry)){
-    fetchYear("http://worldtimeapi.org/api/timezone/Asia/Kolkata");
-    fetchYear("https://timeapi.io/api/Time/current/zone?timeZone=Asia/Kolkata");
-}
-else{
-    document.getElementById("year").textContent = yearData.year;
+if (!cached || sysTime.getTime() > cached.expiry) {
+    fetchYear("https://worldtimeapi.org/api/timezone/Asia/Kolkata");
+} else {
+    document.getElementById("year").textContent = cached.year;
 }
 
+//fetch year
 async function fetchYear(endpoint) {
-    while (data == null) {
-        try {
-	    const res = await fetch(endpoint);
-	    data  = await res.json();
-	    let year  = null;
-	    /*
-	      wordtimeapi - data.datetime
-	      timeapi     - data.year
-	    */
-	    if(res.url == "http://worldtimeapi.org/api/timezone/Asia/Kolkata"){
-		year = new Date(data.datetime).getFullYear();
-	    }
-	    else if(res.url == "https://timeapi.io/api/Time/current/zone?timeZone=Asia/Kolkata"){
-		year = data.year;
-	    }
-	    document.getElementById("year").textContent = year;
-	    localStorage.removeItem('yearData');
-	    const obj = {
-		year   : year,
-		expiry : (sysDateTime.getTime()+(24*60*60*1000)) //expiry time is one-day
-	    }
-	    localStorage.setItem('yearData',JSON.stringify(obj));
-	    break;
+    let attempt = 0;
+    while (attempt < 200) {
+        attempt++;
+	try {
+	    const controller = new AbortController();
+	    const timeout = setTimeout(() => controller.abort(), 2000);
 
-        } catch (e) {}
+	    const response = await fetch(endpoint, {
+                method: "GET",
+                signal: controller.signal
+	    });
+	    clearTimeout(timeout);
+
+	    const data = await response.json();
+	    const year = new Date(data.datetime).getFullYear();
+	    updateYear(year);
+	    return;
+
+        } catch (err) {}
     }
+    updateYear(new Date().fullYear());
+}
+
+// write year
+function updateYear(year) {
+    document.getElementById("year").textContent = year;
+    const oneDay = 24 * 60 * 60 * 1000; 
+    const cacheData = {
+        year  : year,
+        expiry: sysTime.getTime() + oneDay
+    };
+
+    localStorage.setItem("yearData", JSON.stringify(cacheData));
 }
